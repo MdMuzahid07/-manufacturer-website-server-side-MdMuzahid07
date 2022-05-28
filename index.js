5e0bc971a3e22d3df9be817432bdddd23f1d9e21
MdMuzahid07 <MdMuzahid7396@gmail.com>
2022-05-28 18:31:54 +0600

    jwt created

--- index.js
+++ index.js
@@ -1,8 +1,9 @@
 const express = require('express');
 const app = express();
+const jwt = require('jsonwebtoken');
 require("dotenv").config();
 const cors = require('cors');
-// const jwt = require('jsonwebtoken')
+const { MongoClient, ServerApiVersion, ObjectId } = require('mongodb');
 const stripe = require('stripe')(process.env.STRIPE_SECRET_KEY)
 const port = process.env.PORT || 5000;
 
@@ -10,11 +11,27 @@ const port = process.env.PORT || 5000;
 app.use(cors());
 app.use(express.json());
 
+// jwt token verify function
+const verifyJWT = (req, res, next) => {
+  const authHeader = req.headers.authorization;
+
+  if (!authHeader) {
+    return res.status(401).send({ message: 'Unauthorized access!' })
+  }
+
+  const token = authHeader.split(' ')[1];
+  jwt.verify(token, process.env.ACCESS_TOKEN_SECRET, function (err, decoded) {
+    if (err) {
+      return res.status(403).send({ message: 'Forbidden access!' })
+    }
+    req.decoded = decoded
+    next()
+  });
+
+}
 
 
 
-
-const { MongoClient, ServerApiVersion, ObjectId } = require('mongodb');
 const { ObjectID } = require('bson');
 const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASSWORD}@cluster0.5yfca.mongodb.net/?retryWrites=true&w=majority`;
 const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology: true, serverApi: ServerApiVersion.v1 });
@@ -37,18 +54,50 @@ const run = async () => {
 
 
 
+
+    // to make admin
+    app.put('/user/admin/:email', verifyJWT, async (req, res) => {
+      const email = req.params.email;
+
+      const requester = req.decoded.email;
+      const requesterAccount = await userCollections.findOne({ email: requester });
+
+      if (requesterAccount.role === 'admin') {
+        const filter = { email: email };
+        const updatedDoc = {
+          $set: { role: 'admin' }
+        };
+        const result = await userCollections.updateOne(filter, updatedDoc);
+        res.send(result);
+      }
+      else {
+        res.status(403).send({ message: 'Forbidden!' })
+      }
+    })
+
+
     // make or update user
-    app.put('/user/:email', async(req, res) => {
+    app.put('/user/:email', async (req, res) => {
       const email = req.params.email;
       const user = req.body;
-      const filter = {email: email};
-      const options = {upsert: true};
+      const filter = { email: email };
+      const options = { upsert: true };
       const updatedDoc = {
         $set: user
       };
 
       const result = await userCollections.updateOne(filter, updatedDoc, options);
-      res.send(result);
+      const token = jwt.sign({ email: email }, process.env.ACCESS_TOKEN_SECRET, { expiresIn: '7h' });
+
+      res.send({ result, token });
+    })
+
+
+
+    // to get add user
+    app.get('/user', verifyJWT, async (req, res) => {
+      const users = await userCollections.find({}).toArray();
+      res.send(users);
     })
 
 
@@ -84,9 +133,27 @@ const run = async () => {
     })
 
     // to get all orders
-    app.get('/order', async (req, res) => {
+    app.get('/order', verifyJWT, async (req, res) => {
+      const email = req.query.email;
+      const decodedEmail = req.decoded.email;
+
+      if (email === decodedEmail) {
+        const query = { email: email };
+        const result = await orderCollection.find(query).toArray();
+        return res.send(result);
+      }
+      else {
+        return res.status(404).send({ message: 'Forbidden access!' })
+      }
+
+    })
+
+
+    // to manage order for admin
+    app.get('/adminOrderManage', async (req, res) => {
       const result = await orderCollection.find({}).toArray();
-      res.send(result);
+      return res.send(result);
+
     })
 
     // to get order by id
