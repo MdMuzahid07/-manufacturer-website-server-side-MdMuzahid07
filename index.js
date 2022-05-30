350502f82b8997fbf23c2421c552b4839e265847
MdMuzahid07 <MdMuzahid7396@gmail.com>
2022-05-30 20:35:38 +0600

    api coded for remove users

--- index.js
+++ index.js
@@ -110,6 +110,14 @@ const run = async () => {
       res.send(users);
     })
 
+    app.delete('/user/:id', async (req, res) => {
+      const id = req.params.id;
+      const query = { _id: ObjectId(id) };
+      const result = await userCollections.deleteOne(query);
+      res.send(result)
+    })
+
+
 
 
     // to get all product
